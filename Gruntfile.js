8bcb993ed8d590e2b17ce0eb1a9dbe574c89c52f
nhatlan <vanphamnhatlan93@gmail.com>
2016-01-10 15:02:38 +0700

    feat cover resp image in markup

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,9 +32,82 @@ module.exports = function(grunt) {
         }]
       }
     },
-  });
+  }
+
+  {
+    responsive__cover_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          newsFileOnly: true, 
+          sizes: [{
+            name:"portrait-extra-small",
+            width: 320,
+            height: 480, 
+            aspectRatio: false, 
+            gravity: center, 
+          },
+          {
+            name:"portrait-small",
+            width: 360,
+            height: 640,
+            aspectRatio: false, 
+            gravity: center, 
+          },
+          {
+            name:"portrait-medium",
+            width: 768,
+            height: 1024,
+            aspectRatio: false, 
+            gravity: center, 
+          },
+          {
+            name:"portrait-large",
+            width: 980,
+            height: 1280,
+            aspectRatio: false, 
+            gravity: center, 
+          },
+          {
+            name:"landscape-small", 
+            width: 360,
+            aspectRatio: true, 
+          },
+          {
+            name:"landscape-medium", 
+            width: 640,
+            aspectRatio: true, 
+          },
+          {
+            name:"landscape-large", 
+            width: 1024,
+            aspectRatio: true, 
+          },
+          {
+            name:"landscape_extra-large", 
+            width: 1920,
+            aspectRatio: true, 
+          }
+          ]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'framework/css/img_cover_src/',
+          dest: 'framework/css/img_cover/'
+        }]
+      }
+    },
+  }
+
+  );
   
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['responsive_images']);
+  grunt.registerTask('default', ['responsive_images, responsive__cover_images']);
 
 };
